9107130b33d1177f07aaf5ca8f9f23185ac8cf5b
Idly App User <idlyappuser@Alices-MacBook-Air.local>
2013-06-30 20:03:07 -0400

    Initial Commit

---
+++ idly/PKOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PKOAppDelegate.h
+//  idly
+//
+//  Created by Idly App User on 6/30/13.
+//  Copyright (c) 2013 Gage IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PKOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ idly/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  idly
+//
+//  Created by Idly App User on 6/30/13.
+//  Copyright (c) 2013 Gage IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PKOAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PKOAppDelegate class]));
+    }
+}
